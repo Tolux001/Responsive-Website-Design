bb6b58f8625f7bbd903c23ef1814962e0bd0eced
Toluwalase Adejuwon <toluwalasepeter100@gmail.com>
2023-12-01 17:33:09 +0100

    update commit

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const nav = document.querySelector("nav");
+const menu_icon = document.querySelector('nav.mobile--nav .menu-icon img');
+const close_icon = document.querySelector('.close--icon');
+const nav_bar = document.querySelector('.nav--bar--mobile');
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 200) {
@@ -6,4 +9,12 @@ window.addEventListener("scroll", () => {
     } else {
         nav.classList.remove("scrolled");
     }
+})
+
+menu_icon.addEventListener(click, ()=> {
+    nav_bar.classList.add("active");
+})
+
+close_icon.addEventListener(click, ()=> {
+    nav_bar.classList.remove("active");
 })
