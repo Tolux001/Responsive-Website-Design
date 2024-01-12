4c035228a47dbb11ed263baa3b4e9b0b87425109
Toluwalase Adejuwon <toluwalasepeter100@gmail.com>
2024-01-12 16:39:17 +0100

    fix Bug

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const close_icon = document.querySelector('.close--icon');
 const nav_bar = document.querySelector('.nav--bar--mobile');
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 200) {
+    if(window.scrollY > 70) {
         nav.classList.add("scrolled");
     } else {
         nav.classList.remove("scrolled");
     }
 })
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 200) {
+    if(window.scrollY > 80) {
         nav_desktop.classList.add("scrolled");
     } else {
         nav_desktop.classList.remove("scrolled");
@@ -26,3 +26,4 @@ menu_icon.addEventListener('click', ()=> {
 close_icon.addEventListener('click', ()=> {
     nav_bar.classList.remove("active");
 })
+
